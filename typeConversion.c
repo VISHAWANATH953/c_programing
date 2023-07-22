04ffd7ac9feceb9e3e836a7b499a41e447837798
Vishawanath kokare <61642625+VISHAWANATH953@users.noreply.github.com>
2023-07-22 10:47:32 +0530

    ADD : implicit conversions

---
+++ typeConversion.c
@@ -0,0 +1,12 @@
+#include <std.io>
+
+void main(){
+
+    //// This are implicit conversions
+    // if we add integers in float it will automatically converts to float
+    float a = 9; // results 9.00
+
+    // if we add float to integers it will automatically converts to integers
+    int b = 9.99; // results 9
+
+}
