d5b8533c9d400145d7e2924432de14ede2c381be
VISHAWANATH953 <G0dB3liv3er@protonmail.com>
2023-07-26 10:11:56 +0100

    ADD: Learned scanf

--- variables.c
+++ variables.c
@@ -1,12 +1,13 @@
-#include<stdio.h>;
+#include<stdio.h>
 
-int main(){
+int main()
+{
     // write variables:- type initialName = value;
 
     // integers(Numbers)
     // stores whole number without decimals
     int a = 0;
-    
+
     // float
     // Stores fractional numbers, containing one or more decimals. stores 6-7 decimal digit
     float b = 75.25;
@@ -15,12 +16,12 @@ int main(){
     // Stores fractional numbers, containing one or more decimals. stores 15 decimal digit
     double d = 35.343434343;
 
-    //char(character)
-    // char use with single quote string only
+    // char(character)
+    //  char use with single quote string only
     char c = 'c';
 
-    //string
-    // string stores with array and also with double quotes
+    // string
+    //  string stores with array and also with double quotes
     char s[] = "vish";
 
     /* basic format specifiers
@@ -30,5 +31,6 @@ int main(){
         %c for char
         %s for string
     */
-    printf("%i\n%f\n%lf\n%c\n%s",a,b,d,c,s);
+    printf("my username is %s\n", s);
+    printf("%i\n%f\n%lf\n%c\n%s", a, b, d, c, s);
 }
