620abdff6379d617cb196062ab19b3babfa66b89
VISHAWANATH953 <G0dB3liv3er@protonmail.com>
2023-07-28 12:02:17 +0100

    ADD: Project countNumber.c

---
+++ projects/countNumber.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main(){
+    int n,i=0;
+    printf("Enter Number: \n");
+    scanf("%d",&n);
+
+    while (i<=n)
+    {
+        printf("%d \n",i);
+        i++;
+    }
+    
+}
